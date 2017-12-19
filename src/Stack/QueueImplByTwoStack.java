2a5477eb576425a2b0f2d5a084782d23b26a1f2d
shao <shao@mrsgx.com>
2017-12-19 16:50:10 +0800

    添加用两个栈实现队列代码

---
+++ src/Stack/QueueImplByTwoStack.java
@@ -0,0 +1,24 @@
+package Stack;
+
+import java.util.Stack;
+
+public class QueueImplByTwoStack {
+	Stack<Integer> in = new Stack<Integer>();
+	Stack<Integer> out = new Stack<Integer>();
+	
+	public void push(int node) {
+		in.push(node);
+	}
+	
+	public int pop() {
+		while(!in.isEmpty()) {
+			out.push(in.pop());
+		}
+		
+		int result= out.pop();
+		while(!out.isEmpty()) {
+			in.push(out.pop());
+		}
+		return result;
+	}
+}
