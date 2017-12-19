1a4aa9a960cf51a9bbb5b0f205f53a3942ce38f4
shao <shao@mrsgx.com>
2017-12-19 18:04:40 +0800

    又加了几道题啊

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,15 @@
 import Stack.QueueImplByTwoStack;
 import Tree.ReConstructBinaryTree;
 import Tree.TreeNode;
+import binary.CountOneOfNumber;
+import dynamicPrograming.Finbonacci;
+import dynamicPrograming.RectCover;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		System.out.println(new CountOneOfNumber().NumberOf1(2147483647));
 	
 	}
 	
