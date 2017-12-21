1c20d561d13a67090e3fd5918b8b821773c1ccc9
shao <shao@mrsgx.com>
2017-12-21 18:03:38 +0800

    1.做了几道关于二叉树的题，感觉全忘了，我写了备注

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
 import Stack.QueueImplByTwoStack;
+import Tree.HasSubtree;
+import Tree.MirrorOfTree;
 import Tree.ReConstructBinaryTree;
 import Tree.TreeNode;
 import array.AjustArray;
@@ -8,14 +10,22 @@ import dynamicPrograming.RectCover;
 import linkedlist.ListNode;
 import linkedlist.ReverseList;
 import math.ExponentOfBase;
+import math.printMatrixByClockWise;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		ListNode node=new ListNode(new int[] {1,2,3,4,5,6,7});
-		node.printSelf();
-		new ReverseList().reverseList(node);
+		//ListNode node=new ListNode(new int[] {1,2,3,4,5,6,7});
+		//new int[][] {{1,2,3,4,5,6,7}}
+		//new int[][] {{1},{2},{3},{4},{5}}
+		//new int[][]{{1,2},{3,4},{5,6},{7,8},{9,10}}
+		//new int[][] {{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}}
+		//new int[][]{{1,2,3,4,5},{6,7,8,9,10},{11,12,13,14,15}}
+		//new int[][]{{1,2,3,4,5},{6,7,8,9,10},{11,12,13,14,15},{16,17,18,19,20},{21,22,23,24,25}}
+		//new printMatrixByClockWise().printMatrix(new int[][]{{1,2,3,4,5},{6,7,8,9,10},{11,12,13,14,15},{16,17,18,19,20},{21,22,23,24,25}});
+		new printMatrixByClockWise().printMatrix(new int[][]{{1,2},{3,4},{5,6},{7,8},{9,10}});
+		
 		System.out.println();
 	
 	}
--- src/Tree/TreeNode.java
+++ src/Tree/TreeNode.java
@@ -2,56 +2,72 @@ package Tree;
 
 public class TreeNode {
 	int val;
-	TreeNode left=null;
-	TreeNode right=null;
+	public TreeNode left = null;
+	public TreeNode right = null;
+
 	public TreeNode(int x) {
-		val=x;
+		val = x;
 	}
+	/**
+	 * 生成一颗二叉树
+	 * @param arr
+	 */
+	public TreeNode(char[] arr) {
+		
+	}
+
 	public void printSelf() {
 		System.out.print("先序遍历为:");
 		printPreTree(this);
 		System.out.println();
-		
+
 		System.out.print("中序遍历为:");
 		printInTree(this);
 		System.out.println();
-		
+
 		System.out.print("后序遍历为:");
 		printTailTree(this);
 		System.out.println();
 	}
-	
+
 	/**
 	 * 先序遍历
+	 * 
 	 * @param root
 	 */
 	private void printPreTree(TreeNode root) {
-		if(root==null) {
+		if (root == null) {
 			return;
 		}
-		System.out.print(root.val+" ");
+		System.out.print(root.val + " ");
 		printPreTree(root.left);
 		printPreTree(root.right);
 	}
+
 	/**
 	 * 中序遍历
+	 * 
 	 * @param root
 	 */
 	private void printInTree(TreeNode root) {
-		if(root==null) return;
+		if (root == null)
+			return;
 		printInTree(root.left);
-		System.out.print(root.val+" ");
+		System.out.print(root.val + " ");
 		printInTree(root.right);
 	}
+
 	/**
 	 * 后序遍历
+	 * 
 	 * @param root
 	 */
 	private void printTailTree(TreeNode root) {
-		if(root==null) return;
+		if (root == null)
+			return;
 		printTailTree(root.left);
 		printTailTree(root.right);
-		System.out.print(root.val+" ");
+		System.out.print(root.val + " ");
 	}
-	
+
 }
